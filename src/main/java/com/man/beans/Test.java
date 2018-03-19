2f7ea2596f4a71f42e413e936621dc0cfec9df01
gitliuhongbing <liuhb@guahao.com>
2018-03-19 12:12:05 +0800

    yes

---
+++ src/main/java/com/man/beans/Test.java
@@ -0,0 +1,11 @@
+package com.man.beans;
+
+/**
+ * Created by liuhongbing on 2018/3/19.
+ */
+public class Test {
+    public  void  test()
+    {
+        System.out.println(System.currentTimeMillis());
+    }
+}
