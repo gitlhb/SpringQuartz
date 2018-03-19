9cef11fa45247a9d9796bfb37eb1f34f899bb4d1
gitliuhongbing <liuhb@guahao.com>
2018-03-19 12:31:20 +0800

    yes

--- src/main/java/com/man/beans/Test.java
+++ src/main/java/com/man/beans/Test.java
@@ -1,11 +1,20 @@
 package com.man.beans;
 
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by liuhongbing on 2018/3/19.
  */
+
+@Component
 public class Test {
-    public  void  test()
+    public  void  testQuartz()
     {
-        System.out.println(System.currentTimeMillis());
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String now = simpleDateFormat.format(new Date());
+        System.out.println(now);
     }
 }
